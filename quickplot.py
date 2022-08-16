9c9a8fa8b1fa1db86ff72855805ff816fdccbfb2
harrylerman <66405902+harrylerman@users.noreply.github.com>
2022-08-16 16:54:29 -0400

    Added delimiter input argument
    Added delimiter input argument

--- quickplot.py
+++ quickplot.py
@@ -16,23 +16,28 @@ def getparams(argv):
     to plot.
     '''
     inputfile = ''
+    delim = '|'
     params = ''
     try:
-        opts, args = getopt.getopt(argv, "hi:p:")
+        opts, args = getopt.getopt(argv, "hi:d:p:")
     except getopt.GetoptError:
-        print('quickplot.py -i <inputfile> -p <parameters>')
+        print('quickplot.py -i <inputfile> -d <delimiter> -p <parameters>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('quickplot.py -i <inputfile> -p <parameters>')
+            print('quickplot.py -i <inputfile> -d <delimiter> -p <parameters>')
             sys.exit()
         elif opt in ("-i"):
             inputfile = arg
+        elif opt in ("-d"):
+            delim = arg
+            if delim == 'tab':
+                delim = '\t'
         elif opt in ("-p"):
             params = arg.split()
-    return inputfile, params
+    return inputfile, delim, params
 
-def readlog(file, params):
+def readlog(file, params, delim):
     '''
     Reads in a csv-converted tlog file (should be converted with mavlogparse script)
     Returns a list of timestamps (in unix epoch time)
@@ -41,15 +46,14 @@ def readlog(file, params):
     '''    
     #open the file and use Python's csv module to read it:
     csvfile = open(file, 'r')
-    reader = csv.DictReader(csvfile, delimiter='\t')   
+    reader = csv.DictReader(csvfile, delimiter=delim)   
     plots = [[] for i in range(len(params))] #initialize a list of empty lists
     time = []
     #read in whatever columns we want according to the column header label:
     for row in reader:
         time.append(float(row['timestamp']))
         for i in range(len(plots)): #iterate over desired plotting parameters
-            plots[i].append(float(row[params[i]]))
-   
+            plots[i].append(float(row[params[i]]))   
     #close the file since we're done reading it:
     csvfile.close()
     return time, plots
@@ -71,13 +75,8 @@ def plotlog(time,plots,params):
         plt.show()        
     return True
 
-inputfile, params = getparams(sys.argv[1:])
+inputfile, delim, params = getparams(sys.argv[1:])
 filepath = 'C:/Users/harry/OneDrive/Documents/McGill stuff/dronescripts/'
 file = filepath+inputfile
-time, plots = readlog(file, params)
+time, plots = readlog(file, params, delim)
 plotlog(time,plots,params)
-
-
-
-
-
